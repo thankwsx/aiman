48e55a49ed1809d264d286983d3fc3f21091780c
thankwsx <thankwsx@gmail.com>
2024-12-25 20:05:36 +0800

    feat: 测试sw

---
+++ public/app/sw.js
@@ -0,0 +1,37 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.5.4/workbox-sw.js');
+
+workbox.setConfig({
+  debug: true,
+});
+
+// Establish a cache name
+const cacheName = "myimages-v1";
+
+// Assets to precache
+const precachedAssets = [
+  "https://alpha.adbi.moonton.net/e74e41f11a96a1402330.png",
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(cacheName).then(cache => {
+    return cache.addAll(precachedAssets);
+  }));
+});
+
+self.addEventListener("fetch", event => {
+  const isPrecachedRequest = precachedAssets.includes(event.request.url);
+
+  if (isPrecachedRequest) {
+    event.respondWith(caches.open(cacheName).then(cache => {
+      console.log("Cache only request for: " + event.request.url);
+
+      return cache.match(event.request.url);
+    }));
+  } else {
+    console.log("Network only request for: " + event.request.url);
+
+    return;
+  }
+});
+
+
